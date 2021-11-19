0145a01b2af3ff4b224e955f95b6bb403ccc7899
user42 <jorge.graton8@gmail.com>
2021-11-19 01:18:23 +0100

    func

--- aps/SelectionSort/SelectionSort.c
+++ aps/SelectionSort/SelectionSort.c
@@ -1,48 +1,33 @@
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //clock(), CLOCKS_PER_SEC e clock_t
 
 
-
-int *SelectionSort(int *a){
-
-
-	int j = 0;
-	int k = 1;
-	int n = 0;
-	int tmp;
-	int len;
-
-	while(a[n] != '\0'){
-		n++;
-	}
-	len = n;
-
-	while(1){
-
-		if(k == len){
-			k = 0;
-			j++;
+void selectionSort(int array[], int len) {
+	int i, j, m, tmp;
+	for (i = 0; i < (len-1); i++)
+	{
+		m = i;
+		for (j = (i+1); j < len; j++) {
+		if(array[j] < array[m])
+			m = j;
 		}
-
-		if(a[j] > a[k]){
-			tmp = a[j];
-			a[j] = a[k];
-			a[k] = tmp;
+		if (i != m) {
+		tmp = array[i];
+		array[i] = array[m];
+		array[m] = tmp;
 		}
-		k++;
-		if(j == len - 1)
-			break;
 	}
-	return a;
 }
+
 
 int main() {
 
 	int a[] = {1, 3 , 4 , 8 , 6 , 27 , 49, 43, 431 ,4532 , 4324 ,43 , 54 , 1 ,1, 89, 995, 9986, 9785, 9558, 8754 };
-	int *b = SelectionSort(a);
+	selectionSort(a, 22);
 
 int count = 0;
-
 
 	for(int i = 0; i < 22 ; i++){
 		printf("%d\n", a[i]);
